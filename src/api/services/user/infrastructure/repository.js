be1e44a793c8f0f114fe5d0f2ed095ba755eb507
Chanyeop Jeong <devops3199@gmail.com>
2021-08-15 22:18:53 +0900

    feat: create email repository

--- src/api/services/user/infrastructure/repository.js
+++ src/api/services/user/infrastructure/repository.js
@@ -30,6 +30,27 @@ export class UserRepository {
         return result.length > 0 ? result[0] : ['No Result'];
     }
 
+    async findByEmail(email) {
+        const connection = mysql.createConnection(dbConnection);
+        
+        const getUser = (email) => {
+            return new Promise((resolve, reject) => {
+                connection.query(`SELECT email, password FROM user WHERE email = ${email}`, (error, results, fields) => {
+                    if (error) {
+                        reject(new Error('Request Query Error - get a user'));
+                    }
+                    resolve(results);
+                });
+            })
+        }
+
+        const result = await getUser(id);
+
+        connection.end();
+
+        return result.length > 0 ? result[0] : ['No Result'];
+    }
+
     async save(user) {
         const connection = mysql.createConnection(dbConnection);
 
