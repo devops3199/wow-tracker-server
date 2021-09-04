8e1716348a62408d2e023be56507809eecbe09c9
Chanyeop Jeong <devops3199@gmail.com>
2021-09-04 20:44:07 +0900

    chore: add comment

--- src/api/routes/user/index.js
+++ src/api/routes/user/index.js
@@ -7,8 +7,9 @@ const user = new Router();
 
 user.get('/:userId', async (ctx, next) => {
     const id = Number(ctx.params.userId);
-    const userRepository = new UserRepository();
-    const result = await userRepository.findById(id);
+    // const userRepository = new UserRepository();
+    // const result = await userRepository.findById(id);
+    // Service로 대채
     ctx.body = result;
 });
 
