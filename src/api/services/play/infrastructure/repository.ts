20eff6366a6f144020c32bc90bd339753a8f7b46
Ray Jeong <devops3199@gmail.com>
2021-10-03 19:14:44 +0900

    refactor: js -> ts

--- src/api/services/play/infrastructure/repository.ts
+++ src/api/services/play/infrastructure/repository.ts
@@ -1,36 +1,36 @@
 import mysql from 'mysql';
-import { dbConnection } from '../../../../shared/config.js';
+import { dbConnection } from '../../../../shared/config';
+import { Play } from '../domain/model';
 
 export class playRepository {
     async findAll() {
         const connection = mysql.createConnection(dbConnection);
 
-        const getAllPlay = () => {
+        const getAllPlay = (): Promise<string[]> => {
             return new Promise((resolve, reject) => {
                 connection.query(`SELECT date, begin, end, dungeon, raid FROM play OREDER BY date)`, (error, results, fields) => {
                     if (error) {
                         reject(new Error('Request Query Error - get all play time'));
                     }
-                    resolve();
+                    resolve(results);
                 });
             });
         }
-
-        await getAllPlay();
-
         connection.end();
+
+        return await getAllPlay();
     }
     
-    async save(play) {
+    async save(play: Play) {
         const connection = mysql.createConnection(dbConnection);
 
-        const savePlay = (play) => {
+        const savePlay = (play: Play): Promise<void> => {
             return new Promise((resolve, reject) => {
                 connection.query(`INSERT INTO play (date, begin, end, dungeon, raid) VALUES (${play.date}, ${play.begin}, ${play.end}, ${play.dungeon}, ${play.raid})`, (error, results, fields) => {
                     if (error) {
                         reject(new Error('Request Query Error - save play time'));
                     }
-                    resolve();
+                    resolve(results);
                 });
             });
         }
