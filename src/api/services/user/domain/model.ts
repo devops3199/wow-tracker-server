923268ad48a693ffc32652f2be3fb9359edb9e88
Ray Jeong <devops3199@gmail.com>
2021-11-06 19:31:27 +0900

    refactor: user model

--- src/api/services/user/domain/model.ts
+++ src/api/services/user/domain/model.ts
@@ -3,37 +3,29 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  readonly id!: number;
+  id!: number;
 
   @Column()
-  readonly _email: string;
+  email: string;
 
   @Column()
-  private _name: string;
+  name: string;
 
   @Column()
-  private _password: string;
+  password: string;
 
-  constructor(args: { email: string; name: string; password: string }) {
-    this._email = args.email;
-    this._name = args.name;
-    this._password = args.password;
+  @Column()
+  createdAt: Date;
+
+  constructor(args: { email: string; name: string; password: string; createdAt: Date }) {
+    this.email = args.email;
+    this.name = args.name;
+    this.password = args.password;
+    this.createdAt = args.createdAt;
   }
 
-  get password() {
-    return this._password;
-  }
-
-  set password(password: string) {
+  hashPassword(password: string) {
     if (typeof password !== 'string') throw new Error('Password Hash Error');
-    this._password = password;
-  }
-
-  get email() {
-    return this._email;
-  }
-
-  get name() {
-    return this._name;
+    this.password = password;
   }
 }
