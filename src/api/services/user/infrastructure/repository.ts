783807bc0f5dcd1e6be41be522b3b00572f399fb
Ray Jeong <devops3199@gmail.com>
2021-11-07 20:40:40 +0900

    chore: remove import

--- src/api/services/user/infrastructure/repository.ts
+++ src/api/services/user/infrastructure/repository.ts
@@ -1,7 +1,5 @@
-import mysql from 'mysql';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../domain/model';
-import { dbConnection } from '../../../../shared/config';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
