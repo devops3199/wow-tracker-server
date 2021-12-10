cb421b4073dbf02179d82e1e2ee86e2247197c03
Ray Jeong <devops3199@gmail.com>
2021-12-10 22:49:41 +0900

    feat: auth middleware

--- src/middleware/auth/index.ts
+++ src/middleware/auth/index.ts
@@ -1,6 +1,15 @@
 import { Context } from 'koa';
+import jwt from 'jsonwebtoken';
 
 export const authMiddleware = async (ctx: Context, next: () => Promise<any>) => {
-  console.log(ctx.state);
+  const token = ctx.request.header.authorization;
+
+  if (!token) {
+    // FIXME: throw 403
+    throw Error();
+  }
+  // @ts-ignore
+  const { id } = jwt.decode(token);
+
   await next();
 };
