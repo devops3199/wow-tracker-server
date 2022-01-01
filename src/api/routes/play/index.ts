bc6ff0f97a2c21055c32e298813c86903be3a5ef
Ray Jeong <devops3199@gmail.com>
2022-01-01 18:54:49 +0900

    chore: comment

--- src/api/routes/play/index.ts
+++ src/api/routes/play/index.ts
@@ -3,8 +3,7 @@ import { PlayService } from '../../services/play/application/service';
 
 const play = new Router();
 
-play.get('/:userId', async (ctx, next) => {
-  // FIXME: through get user from auth middleware
+play.get('/:userId', async (ctx) => {
   const { userId: id } = ctx.state as { userId: number };
   const service = new PlayService();
   const plays = service.getPlay(id);
