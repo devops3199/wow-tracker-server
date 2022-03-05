4fba8bc1ca150dffc433669fb278c85180c6456b
Ray Jeong <devops3199@gmail.com>
2022-03-06 08:20:01 +0900

    refactor: route

--- src/api/routes/bnet/index.ts
+++ src/api/routes/bnet/index.ts
@@ -6,41 +6,42 @@ const bnet = new Router();
 bnet.get('/profile', async (ctx, next) => {
   // @ts-ignore
   const user = ctx.req.user;
-  if (user) {
-    http.setAuthorization(`Bearer ${user.token}`);
-    http.setBattlenetNamespace('profile-kr');
 
-    const result = await http.get('/profile/user/wow');
-
-    ctx.status = 200;
-    ctx.body = { data: result };
-  } else {
+  if (!user) {
     ctx.status = 403;
     ctx.body = 'Invalid session';
   }
+
+  http.setAuthorization(`Bearer ${user.token}`);
+  http.setBattlenetNamespace('profile-kr');
+
+  const result = await http.get('/profile/user/wow');
+
+  ctx.status = 200;
+  ctx.body = { data: result };
 });
 
 bnet.get('/realms', async (ctx, next) => {
   // @ts-ignore
   const user = ctx.req.user;
 
-  if (user) {
-    http.setAuthorization(`Bearer ${user.token}`);
-    http.setBattlenetNamespace('dynamic-kr');
-
-    const result = await Promise.all([
-      http.get('/data/wow/connected-realm/205'),
-      http.get('/data/wow/connected-realm/210'),
-      http.get('/data/wow/connected-realm/214'),
-      http.get('/data/wow/connected-realm/2116'),
-    ]);
-
-    ctx.status = 200;
-    ctx.body = { data: result };
-  } else {
+  if (!user) {
     ctx.status = 403;
     ctx.body = 'Invalid session';
   }
+
+  http.setAuthorization(`Bearer ${user.token}`);
+  http.setBattlenetNamespace('dynamic-kr');
+
+  const result = await Promise.all([
+    http.get('/data/wow/connected-realm/205'),
+    http.get('/data/wow/connected-realm/210'),
+    http.get('/data/wow/connected-realm/214'),
+    http.get('/data/wow/connected-realm/2116'),
+  ]);
+
+  ctx.status = 200;
+  ctx.body = { data: result };
 });
 
 export default bnet;
