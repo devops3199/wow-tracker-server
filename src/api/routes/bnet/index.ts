b1d0a1a03da82b9e79979dbc1a559d08ab23ea3e
Ray Jeong <devops3199@gmail.com>
2022-03-05 23:02:11 +0900

    feat: realms

--- src/api/routes/bnet/index.ts
+++ src/api/routes/bnet/index.ts
@@ -18,4 +18,33 @@ bnet.get('/profile', async (ctx, next) => {
   }
 });
 
+bnet.get('/realms', async (ctx, next) => {
+  // @ts-ignore
+  const user = ctx.req.user;
+
+  if (user) {
+    // FIXME: 더럽다
+    const result = await Promise.all([
+      http.get('/data/wow/connected-realm/205', {
+        headers: { authorization: `Bearer ${user.token}`, 'Battlenet-Namespace': 'dynamic-kr', locale: 'ko_KR' },
+      }),
+      http.get('/data/wow/connected-realm/210', {
+        headers: { authorization: `Bearer ${user.token}`, 'Battlenet-Namespace': 'dynamic-kr', locale: 'ko_KR' },
+      }),
+      http.get('/data/wow/connected-realm/214', {
+        headers: { authorization: `Bearer ${user.token}`, 'Battlenet-Namespace': 'dynamic-kr', locale: 'ko_KR' },
+      }),
+      http.get('/data/wow/connected-realm/2116', {
+        headers: { authorization: `Bearer ${user.token}`, 'Battlenet-Namespace': 'dynamic-kr', locale: 'ko_KR' },
+      }),
+    ]);
+
+    ctx.status = 200;
+    ctx.body = { data: result.map((promise) => promise.data) };
+  } else {
+    ctx.status = 403;
+    ctx.body = 'Invalid session';
+  }
+});
+
 export default bnet;
